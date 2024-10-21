df8d632edcfe19850334ff1ec560e9ceb3ace880
schaiany <165675722+schaiany@users.noreply.github.com>
2024-10-21 11:34:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomy',
+    'Qual a constelação mais famosa pela sociedade?',
+    'A constelação de orion, mais conhecida com as três marias'
+)
+
+criaCartao(
+    'Astronomy',
+    'oque é o vacuo?',
+    'é uma região do espaço totalmente destituída de matéria, um lugar onde não contém nada.'
+)
+
+criaCartao(
+    'Astronomy',
+    'Qyem foi o responsável pela teoria da relatividade?',
+    ' Albert Einstein'
+)
+
+criaCartao(
+    'Astronomy',
+    'qual é a galáxia que está se aproximando de nós',
+    'A galáxia de Andrômeda'
+)
